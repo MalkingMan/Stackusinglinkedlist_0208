e292f840356a288515b6f7978dacf5b0389ca450
MalkingMan <arrayalkhozini@gmail.com>
2024-05-11 10:04:22 +0700

    commit 18

--- Stack.cpp
+++ Stack.cpp
@@ -112,7 +112,10 @@ int main()
             }
             break;
         case 4:
-            cout << "Exiting program." <<  endl;
+                cout << "Exiting program." <<  endl;
+            break;
+            default:
+                cout << "Invalid choice. Try again." << endl;   
             break;
 
 
